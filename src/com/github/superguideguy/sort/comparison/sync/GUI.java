6e6931883336d434a9afa0277c49114a2a5ae6b4
superguideguy <46546836+superguideguy@users.noreply.github.com>
2019-02-04 13:32:46 -0600

    Update GUI.java

--- src/com/github/superguideguy/sort/comparison/sync/GUI.java
+++ src/com/github/superguideguy/sort/comparison/sync/GUI.java
@@ -1,6 +1,10 @@
 package com.github.superguideguy.sort.comparison.sync;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -11,6 +15,7 @@ public class GUI implements Runnable, KeyListener {
 	
 	static JFrame frame;
 	static JPanel mainPanel, sidePanel;
+	final static int mpH = 500, spH = 50, width = 1000;
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
@@ -31,27 +36,35 @@ public class GUI implements Runnable, KeyListener {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Comparison of Sorting Methods for Partial Sorting and other Adaptive Sorting Objectives");
-		frame.setSize(1000,550);
+		frame.setSize(width, mpH+spH);
 		frame.setBackground(Color.BLACK);
 		frame.setForeground(Color.BLACK);
 		
 		mainPanel = new JPanel();
-		mainPanel.setSize(1000, 500);
+		mainPanel.setSize(width, mpH);
 		mainPanel.setBackground(Color.BLACK);
 		frame.add(mainPanel);
 		
 		sidePanel = new JPanel();
-		sidePanel.setSize(1000, 50);
+		sidePanel.setSize(width, spH);
 		sidePanel.setBackground(Color.GRAY);
 		frame.add(sidePanel);
 		
 		frame.setVisible(true);
-		
+		update();
 	}
 	
 	public static void update() {
 		//Draw array (pre-synchronized)
+		Graphics2D g2;
+		Graphics g3;
 		//Draw bottom bar (TimeKeeper.t_cumm, Runner.task, TimeKeeper.current_avgRMS)
+		Image img2 = sidePanel.createVolatileImage(width, spH);
+		g2 = (Graphics2D)img2.getGraphics();
+		g2.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+		g2.drawString(Runner.task.name(), 5+100*(width/300), spH-5);
+		g3 = sidePanel.getGraphics();
+		g3.drawImage(img2, 0, 0, null);
 	}
 	
 	private static void exitExternal() {
