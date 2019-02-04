82002583eb569e0755775229653dedd581842d74
superguideguy <46546836+superguideguy@users.noreply.github.com>
2019-02-03 21:05:21 -0600

    Update TimeKeeper.java

--- src/com/github/superguideguy/sort/comparison/sync/TimeKeeper.java
+++ src/com/github/superguideguy/sort/comparison/sync/TimeKeeper.java
@@ -7,9 +7,13 @@ public class TimeKeeper implements Runnable {
 
 	static Duration t_total, t_cumm;
 	static Instant t_begin, t_end;
+	
 	static long sigma_ms_inversion = 0;
 	static double sigma_ms_avgRMS = 0, current_avgRMS = 0;
 	static int correct_begin = 0, correct_end = 0;
+	
+	public static final int objectiveB = Runner.ARRAY_SIZE+1, objectiveE = Runner.ARRAY_SIZE+1;
+	public static boolean objectiveMet = false;
 	
 	@Override
 	public void run() {
@@ -38,7 +42,9 @@ public class TimeKeeper implements Runnable {
 	
 	static void stop() {
 		t_end = Instant.now();
-		t_cumm = t_cumm.plus(Duration.between(t_begin, t_end));
+		synchronized (t_cumm) {
+			t_cumm = t_cumm.plus(Duration.between(t_begin, t_end));
+		}
 	}
 	
 	static void commit() {
@@ -46,6 +52,7 @@ public class TimeKeeper implements Runnable {
 	}
 	
 	static void update() {
+		//Inversions
 		boolean previousIsDecreasing = false, currentIsDecreasing = false;
 		for (int i = 0; i < Runner.arr.length - 1; i++) {
 			if (i > i + 1) currentIsDecreasing = true;
@@ -54,6 +61,29 @@ public class TimeKeeper implements Runnable {
 			previousIsDecreasing = currentIsDecreasing;
 		}
 		
+		//RMS
+		current_avgRMS = 0;
+		double z = 0;
+		for (int i = 0; i < Runner.arr.length; i++) {
+			double x = Runner.arr[i] - Runner.arr[Runner.arr[i]-1];
+			z += Math.pow(x, 2);
+		}
+		z = z / Runner.arr.length;
+		z = Math.sqrt(z);
+		current_avgRMS = z;
+		sigma_ms_avgRMS += current_avgRMS;
+		
+		//Correct beginning/ending
+		correct_begin = 0;
+		for (int i = 0; i < Runner.arr.length; i++) {
+			if (Runner.arr[i] == i+1) correct_begin++;
+			else break;
+		}
+		for (int i = Runner.arr.length - 1; i >= 0; i--) {
+			if (Runner.arr[i] == i+1) correct_end++;
+			else break;
+		}
+		if ((correct_begin >= objectiveB) || (correct_end >= objectiveE)) objectiveMet = true;
 		
 	}
 	
